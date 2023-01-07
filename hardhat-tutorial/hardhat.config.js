eacacfc9ff4b61d79207f3cebd5a2a368d512ef8
Devin Catuns <deecat10@gmail.com>
2023-01-06 20:58:26 -0600

    save contract address

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -4,7 +4,8 @@ require("dotenv").config({ path: ".env" });
 const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-// Crypto Devs Contract Address: 0x7176C72e34bD0C96e75C9Df5520FBa203d4f2C0E
+// [1] Crypto Devs Contract Address: 0x7176C72e34bD0C96e75C9Df5520FBa203d4f2C0E
+// [2] Crypto Devs Contract Address: 0x85B249C6E95493a87684d7093E1ff825EE10d4cB
 
 module.exports = {
   solidity: "0.8.4",
